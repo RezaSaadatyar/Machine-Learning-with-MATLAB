d7201950c47ee519c7603bba8100bffddfd1ee73
Reza Saadatyar <96347878+RezaSaadatyar@users.noreply.github.com>
2025-03-03 03:41:59 +0330

    Update main.m

--- Code/main.m
+++ Code/main.m
@@ -4,7 +4,7 @@
 % ============================== Email: Reza.Saadatyar@outlook.com =============================
 % ======================================= 2018-2019 ============================================
 
-% Please Run Code Main.m, the Software will run automatically and there is no need to run the 
+% Please Run Code main.m, the Software will run automatically and there is no need to run the 
 % Functions folder codes
 clc;clear;close all;
 addpath(genpath(cd))
